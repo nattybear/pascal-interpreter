e71ae16c29ede72996efea7bd65ec5145aa143de
준규 <vpark45@gmail.com>
2022-03-26 22:37:59 +0900

    Modify parser.py
    * Seperate Lexer class from Interpreter class
    * Support multiplication and division
    * There is bug: it only calculate first two numbers

--- pascal-interpreter/parser.py
+++ pascal-interpreter/parser.py
@@ -1,4 +1,4 @@
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
 
 class Token(object):
   def __init__(self, type, value):
@@ -14,15 +14,14 @@ class Token(object):
   def __repr__(self):
     return self.__str__()
 
-class Interpreter(object):
+class Lexer(object):
   def __init__(self, text):
     self.text = text
     self.pos = 0
-    self.current_token = None
     self.current_char = self.text[self.pos]
 
   def error(self):
-    raise Exception('Invalid syntax')
+    raise Exception('Invalid character')
 
   def advance(self):
     self.pos += 1
@@ -49,39 +48,46 @@ class Interpreter(object):
         continue
       if self.current_char.isdigit():
         return Token(INTEGER, self.integer())
-      if self.current_char == '+':
+      if self.current_char == '*':
         self.advance()
-        return Token(PLUS, '+')
-      if self.current_char == '-':
+        return Token(MUL, '*')
+      if self.current_char == '/':
         self.advance()
-        return Token(MINUS, '-')
+        return Token(DIV, '/')
       self.error()
     return Token(EOF, None)
 
+class Interpreter(object):
+  def __init__(self, lexer):
+    self.lexer = lexer
+    self.current_token = self.lexer.get_next_token()
+
+  def error(self):
+    raise Exception('Invalid syntax')
+
   def eat(self, token_type):
     if self.current_token.type == token_type:
-      self.current_token = self.get_next_token()
+      self.current_token = self.lexer.get_next_token()
     else:
       self.error()
 
-  def term(self):
+  def factor(self):
     token = self.current_token
     self.eat(INTEGER)
     return token.value
 
   def expr(self):
-    self.current_token = self.get_next_token()
-    result = self.term()
-    while self.current_token.type in (PLUS, MINUS):
+    result = self.factor()
+    while self.current_token.type in (MUL, DIV):
       token = self.current_token
-      if token.type == PLUS:
-        self.eat(PLUS)
-        result = result + self.term()
-      elif token.type == MINUS:
-        self.eat(MINUS)
-        result = result - self.term()
-    return result
-
+      if token.type == MUL:
+        self.eat(MUL)
+        result = result * self.factor()
+      elif token.type == DIV:
+        self.eat(DIV)
+        result = result / self.factor()
+      return result
+  
 def main():
   while True:
     try:
@@ -90,7 +96,8 @@ def main():
       break
     if not text:
       continue
-    interpreter = Interpreter(text)
+    lexer = Lexer(text)
+    interpreter = Interpreter(lexer)
     result = interpreter.expr()
     print(result)
 
